7cda0661e7b1b241d3edd733eebeabfbbcefd3f7
Patrick Ziajski <patrick.ziajski@gmail.com>
2019-03-07 20:41:59 -0500

    added multiple functions formating **does not run**

--- main.py
+++ main.py
@@ -1,82 +1,74 @@
 from tkinter import *
+import tkinter.messagebox
 import LabJackPython
 import ue9
 
-# LabJack initialation
-# myUE9 = ue9.UE9(ethernet = True, ipAddress = "10.32.89.")
-# myUE9.getCalibrationData()
-
-# state variables
-
-# states include True(Clockwise) and False(Counter Clockwise)
-motorDirection = True
-
 # functions
-def updateSetSpeedLB(var):
-    setPercentLB['text'] = str(speedSlider.get()) + " %"
+def calcTimeValue(duty, time):
+    time = ((duty * 65536) / 100) - 65536
 
-def toggleDirectionButtons():
-    global motorDirection
-    if motorDirection:
+def updateDutyCyleLB(duty):
+    dutyCyclePercentLB['text'] = str(speedSlider.get()) + " %"
+    duty = speedSlider.get()
+
+def toggleDirectionButtons(var):
+    if var:
         cwButton['state'] = NORMAL
         ccwButton['state'] = DISABLED
-        motorDirection = False
+        var = False
     else:
         ccwButton['state'] = NORMAL
         cwButton['state'] = DISABLED
-        motorDirection = True
+        var = True
 
-# form creation and details
-form = Tk()
-form.title('Motor Control System')
-form.geometry("511x231")
-form.resizable(0,0)
+def manualResetPressed():
+    tkinter.messagebox.showinfo("Attention", "Manual reset has been attempted")
 
-# label creation
-curSpeedLB = Label(form, text = "Current Motor Speed:")
-setSpeedLB = Label(form, text = "Set Motor Speed:")
-curPercentLB = Label(form, text = "0 %")
-setPercentLB = Label(form, text = "0 %")
-directionLB = Label(form, text = "Motor Direction:")
+if __name__ == "__main__":
+    # variables
+    motorDirection = True # states refer to Clockwise(True) and Counter Clockwise(False)
+    dutyCycle = 0
+    timeValue = 0
+    
+    # form creation and details
+    form = Tk()
+    form.title('Motor Control System')
+    form.geometry("511x231")
+    form.resizable(0,0)
 
-# button creation
-updateButton = Button(form, text = "Update Settings", height = 3, width = 35)
-stopButton = Button(form, text = "Emergency Stop", height = 3, width = 35)
-cwButton = Button(form, text = "Clockwise", height = 3, width = 20, state = DISABLED, command = toggleDirectionButtons)
-ccwButton = Button(form, text = "Counter Clockwise", height = 3, width = 20, command = toggleDirectionButtons)
+    # UI elements
+    dutyCycleLB = Label(form, text = "Desired Duty Cycle:")
+    dutyCyclePercentLB = Label(form, text = "0 %")
+    directionLB = Label(form, text = "Motor Direction:")
+    updateButton = Button(form, text = "Update Settings", height = 3, width = 35)
+    stopButton = Button(form, text = "Emergency Stop", height = 3, width = 35)
+    cwButton = Button(form, text = "Clockwise", height = 3, width = 20, state = DISABLED, command = toggleDirectionButtons(motorDirection))
+    ccwButton = Button(form, text = "Counter Clockwise", height = 3, width = 20, command = toggleDirectionButtons(motorDirection))
+    speedSlider = Scale(form, from_ = 0, to = 100, orient = HORIZONTAL, showvalue = 0, length = 200, command = updateDutyCyleLB(dutyCycle))
 
-# slider creation
-speedSlider = Scale(form, from_ = 0, to = 100, orient = HORIZONTAL, showvalue = 0, length = 200, command = updateSetSpeedLB)
+    # UI layout
+    dutyCycleLB.grid()
+    dutyCycleLB.place(x = 11, y = 17)
+    dutyCyclePercentLB.grid()
+    dutyCyclePercentLB.place(x = 120, y = 17)
+    directionLB.grid()
+    directionLB.place(x = 325, y = 17)
+    updateButton.grid()
+    updateButton.place(x = 26, y = 102)
+    stopButton.grid()
+    stopButton.place(x = 26, y = 158)
+    cwButton.grid()
+    cwButton.place(x = 335, y = 46)
+    ccwButton.grid()
+    ccwButton.place(x = 335, y = 102)
+    speedSlider.grid()
+    speedSlider.place(x = 50, y = 46)
 
-# UI layout
-curSpeedLB.grid()
-curSpeedLB.place(x = 11, y = 17)
+    """
+    # LabJack initialation
+    myUE9 = ue9.UE9(ethernet = True, ipAddress = "10.32.89.110")
+    myUE9.getCalibrationData()
+    myUE9.timerCounter(TimerClockBase = 1, TimerClockDivisor = 1, Timer0Mode = 0, NumTimersEnabled = 1, UpdateConfig = 1, Timer0Value = 52000)
+    """
 
-curPercentLB.grid()
-curPercentLB.place(x = 130, y = 17)
-
-setSpeedLB.grid()
-setSpeedLB.place(x = 180, y = 17)
-
-setPercentLB.grid()
-setPercentLB.place(x = 274, y = 17)
-
-directionLB.grid()
-directionLB.place(x = 325, y = 17)
-
-updateButton.grid()
-updateButton.place(x = 26, y = 102)
-
-stopButton.grid()
-stopButton.place(x = 26, y = 158)
-
-cwButton.grid()
-cwButton.place(x = 335, y = 46)
-
-ccwButton.grid()
-ccwButton.place(x = 335, y = 102)
-
-speedSlider.grid()
-speedSlider.place(x = 50, y = 46)
-
-form.mainloop()
+    form.mainloop()
